774b83a9f4fce03009c27bf70d9832c8d0957c9d
unknown <taskony@gmail.com>
2026-03-22 10:03:24 +0100

    Changes

---
+++ dist/_astro/DynamicTool.BrCJa1d0.js
@@ -0,0 +1,1 @@
+import{D as a}from"./DynamicTool.CU5HFMhV.js";import"./lifecycle.BJdTGnHM.js";import"./template.9szqJRjC.js";import"./props.DBHOwN0V.js";/* empty css                        */export{a as default};
